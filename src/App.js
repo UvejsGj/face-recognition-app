77917473233f6afef8d12ba3eb44d5e87a89b1fb
unknown <shiq.une1@gmail.com>
2022-10-28 20:30:58 +0200

    Added TSParticles backgrond

--- src/App.js
+++ src/App.js
@@ -2,14 +2,113 @@
 import './App.css';
 import Navbar from './components/Navbar/navbar';
 import Logo from './components/Logo/logo';
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Tilt from "react-parallax-tilt"
 
-function App() {
-  return (
-    <div className='App'>
+
+  const App = () => {
+    const particlesInit = useCallback(async (engine) => {
+      console.log(engine);
+      await loadFull(engine);
+    }, []);
+  
+    const particlesLoaded = useCallback(async (container) => {
+      await console.log(container);
+    }, []);
+
+    
+
+    return (
+      <div className='App'>
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        // background: {
+        //   color: {
+        //     value: "#0d47a1",
+        //   },
+       // },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+
+  
       <Navbar/>
       <Logo/>
+      <Tilt/>
     </div>
-  )
-}
+      
 
-export default App;
+    );
+  
+};
+
+export default App
--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,11 +1,18 @@
 import React from "react";
 import LogoImg from "./brain.png"
+import './logo.css';
+import Tilt from 'react-parallax-tilt';
 
 const Logo = () => {
     return(
-        <div className="logo shadow-2 w4 h4 ma4 br4 grow pa3 ">
+        <Tilt 
+        className="Tilt"
+        options={{max:55}}
+        style={{height: 250, width: 250}}>
+            <div className="logo shadow-2 w4 h4 mh3 br2 pa4 grow">
             <img src={LogoImg} alt="brain logo"></img>
         </div>
+        </Tilt>
     )
 }
 
